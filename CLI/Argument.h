c4c704ecf1903a7b74a42a22bc035b8530d09661
AslanyanMamikon <mamikon.aslanyan90@gmail.com>
2025-11-01 20:58:05 +0400

    Add files via upload

--- CLI/Argument.h
+++ CLI/Argument.h
@@ -22,7 +22,7 @@ struct ArgumentInfo {
     std::vector<std::string> allowedValues;
 };
 
-/* ArgumentInfo Example
+/*
 ArgumentInfo layoutFlag = {
     "-layout", 
     ArgumentType::Enum, 
@@ -33,57 +33,14 @@ ArgumentInfo layoutFlag = {
 
 class ArgumentValidator {
 public:
-    void addInfo(const ArgumentInfo& info) {
-        infos[info.name] = info;
-    }
-
-    void validate(const std::unordered_map<std::string, std::string>& flags) const {
-        for (const auto& [name, info] : infos) {
-            auto it = flags.find(name);
-
-            if (info.required && it == flags.end()) {
-                throw CommandValidationException("Missing required flag: " + name);
-            }
-
-            if (it == flags.end()) continue;
-
-            const std::string& value = it->second;
-            switch (info.type) {
-            case ArgumentType::Integer:
-                if (!isInteger(value))
-                    throw CommandValidationException("Flag " + name + " expects an integer");
-                break;
-            case ArgumentType::Float:
-                if (!isFloat(value))
-                    throw CommandValidationException("Flag " + name + " expects a float");
-                break;
-            case ArgumentType::Enum:
-                if (std::find(info.allowedValues.begin(), info.allowedValues.end(), value) == info.allowedValues.end())
-                    throw CommandValidationException("Flag " + name + " invalid value: " + value);
-                break;
-            default:
-                break;
-            }
-        }
-    }
+    void addInfo(const ArgumentInfo& info);
+    void validate(const std::unordered_map<std::string, std::string>& flags) const;
 
 private:
     std::unordered_map<std::string, ArgumentInfo> infos;
 
-    static bool isInteger(const std::string& s) {
-        return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
-    }
-    static bool isFloat(const std::string& s) {
-        bool dotSeen = false;
-        for (char c : s) {
-            if (c == '.') {
-                if (dotSeen) return false;
-                dotSeen = true;
-            }
-            else if (!isdigit(c)) return false;
-        }
-        return true;
-    }
+    static bool isInteger(const std::string& s);
+    static bool isFloat(const std::string& s);
 };
 
 #endif // !ARGUMENT_H_
